4aff03d0794978f0eb0a8bc99c2ffc4e687ee599
katsuronim <rereandreas9@gmail.com>
2022-09-07 23:16:08 +0700

    tambah navigation, data dummy, logout

---
+++ app/src/main/java/com/example/ugd1_a_kelompok9/FragmentMain.kt
@@ -0,0 +1,11 @@
+package com.example.ugd1_a_kelompok9
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FragmentMain:Fragment(R.layout.fragment_main) {
+}
