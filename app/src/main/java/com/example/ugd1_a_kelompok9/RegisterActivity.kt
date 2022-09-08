eab0bdc66426f77e140608a7a7f8f4bca0ef64ce
SaviorRammang <almindosaviorrammang@gmail.com>
2022-09-08 14:59:00 +0700

    register_message

--- app/src/main/java/com/example/ugd1_a_kelompok9/RegisterActivity.kt
+++ app/src/main/java/com/example/ugd1_a_kelompok9/RegisterActivity.kt
@@ -3,15 +3,19 @@ package com.example.ugd1_a_kelompok9
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.view.isEmpty
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var email: TextInputEditText
-    private lateinit var noTelp: TextInputEditText
-    private lateinit var tanggalLahir: TextInputEditText
-    private lateinit var username: TextInputEditText
-    private lateinit var password: TextInputEditText
+    private lateinit var email: TextInputLayout
+    private lateinit var noTelp: TextInputLayout
+    private lateinit var tanggalLahir: TextInputLayout
+    private lateinit var username: TextInputLayout
+    private lateinit var password: TextInputLayout
     private lateinit var btnRegister: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,19 +29,37 @@ class RegisterActivity : AppCompatActivity() {
         password = findViewById(R.id.inputPassword)
         btnRegister = findViewById(R.id.btnRegister)
 
-        btnRegister.setOnClickListener{
+        btnRegister.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             val mBundle = Bundle()
-            //memasukkann data ke dalam bundle
-            mBundle.putString("email", email.text.toString())
-            mBundle.putString("noTelp", noTelp.text.toString())
-            mBundle.putString("tanggalLahir", tanggalLahir.text.toString())
-            mBundle.putString("username", username.text.toString())
-            mBundle.putString("password", password.text.toString())
-            //melakukan intent dengan memanggil bundle
-            intent.putExtras(mBundle)
 
-            startActivity(intent)
+
+            if (username.isEmpty()) {
+                username.setError("Nama Tidak Boleh Kosong")
+                if (password.isEmpty()) {
+                    password.setError("Password Tidak Boleh Kosong")
+                    if (noTelp.isEmpty()) {
+                        noTelp.setError("Nomor Telepon Tidak Boleh Kosong")
+                        if (tanggalLahir.isEmpty()) {
+                            tanggalLahir.setError("Tanggal Lahir Tidak Boleh Kosong")
+                            if (email.isEmpty()) {
+                                email.setError("Email Tidak Boleh Kosong")
+
+
+                                //memasukkann data ke dalam bundle
+                                mBundle.putString("email", email.getEditText()?.getText().toString())
+                                mBundle.putString("noTelp", noTelp.getEditText()?.getText().toString())
+                                mBundle.putString("tanggalLahir", tanggalLahir.getEditText()?.getText().toString())
+                                mBundle.putString("username", username.getEditText()?.getText().toString())
+                                mBundle.putString("password", password.getEditText()?.getText().toString())
+                                //melakukan intent dengan memanggil bundle
+                                intent.putExtras(mBundle)
+                                startActivity(intent)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
