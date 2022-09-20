8ef6f606de3331241b0de4dd343b814150779023
katsuronim <rereandreas9@gmail.com>
2022-09-20 21:47:06 +0700

    revisi splash screen hanya keluar saat install

--- app/src/main/java/com/example/ugd1_a_kelompok9/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ugd1_a_kelompok9/SplashScreenActivity.kt
@@ -1,34 +1,67 @@
 package com.example.ugd1_a_kelompok9
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
-import android.content.Intent
-import android.os.Looper
+import android.window.SplashScreen
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.lang.Boolean
+import kotlin.Suppress
 
 
-@Suppress("DEPRECATION")
 class SplashScreenActivity : AppCompatActivity() {
+    lateinit var splashScreen: ConstraintLayout
+    var prevStarted = "yes"
+    override fun onResume() {
+        super.onResume()
+        val sharedpreferences =
+            getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+        if (!sharedpreferences.getBoolean(prevStarted, false)) {
+            val editor = sharedpreferences.edit()
+            editor.putBoolean(prevStarted, Boolean.TRUE)
+            editor.apply()
+            val splashScreen = findViewById<ConstraintLayout>(R.id.splashscreen)
+        } else {
+            moveToSecondary()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+    }
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }, 3000) // 3000 is the delayed time in milliseconds.
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+    fun moveToSecondary() {
+        // use an intent to travel from one activity to another.
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
+
+//@Suppress("DEPRECATION")
+//class SplashScreenActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_splash_screen)
+//
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
+//
+////        Handler(Looper.getMainLooper()).postDelayed({
+////            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+////            startActivity(intent)
+////            finish()
+////        }, 3000) // 3000 is the delayed time in milliseconds.
+//
+//        Handler().postDelayed({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
+//    }
+//}
