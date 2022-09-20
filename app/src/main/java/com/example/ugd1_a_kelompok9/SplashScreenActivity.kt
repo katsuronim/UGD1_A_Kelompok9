3c1b27a308cde2058109069a458b0e160941d415
SaviorRammang <almindosaviorrammang@gmail.com>
2022-09-20 16:12:54 +0700

    Membuat Splash Screen berhasil

--- app/src/main/java/com/example/ugd1_a_kelompok9/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ugd1_a_kelompok9/SplashScreenActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import android.content.Intent
+import android.os.Looper
+
 
 @Suppress("DEPRECATION")
 class SplashScreenActivity : AppCompatActivity() {
@@ -17,11 +19,16 @@ class SplashScreenActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        Handler().postDelayed({
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000) // 3000 is the delayed time in milliseconds.
 
+        Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        }, 3000)
     }
 }
