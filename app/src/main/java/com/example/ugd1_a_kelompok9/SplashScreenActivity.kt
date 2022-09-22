e94dc95b5ab5324239b04bdfde858950d18cbf1b
SaviorRammang <almindosaviorrammang@gmail.com>
2022-09-22 15:30:06 +0700

    Memperbaiki error pada splash screen

--- app/src/main/java/com/example/ugd1_a_kelompok9/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ugd1_a_kelompok9/SplashScreenActivity.kt
@@ -19,16 +19,17 @@ class SplashScreenActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }, 3000) // 3000 is the delayed time in milliseconds.
-
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
     }
-}
+}// 3000 is the delayed time in milliseconds.
+
+//        Handler().postDelayed({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
+//    }
