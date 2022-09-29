ae3ffc88e6d85f7cb078da07b991481f9e798596
katsuronim <rereandreas9@gmail.com>
2022-09-29 07:30:45 +0700

    commit add crud for destination

--- app/src/main/java/com/example/ugd1_a_kelompok9/room/Destination.kt
+++ app/src/main/java/com/example/ugd1_a_kelompok9/room/Destination.kt
@@ -1,14 +1,4 @@
 package com.example.ugd1_a_kelompok9.room
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class Destination (
-        @PrimaryKey(autoGenerate = true)
-        val destID : Int,
-        val placeName : String,
-        val tglBerangkat : String,
-        val tglPulang : String
-//        val cost : Int
-    )
+class Destination {
+}
